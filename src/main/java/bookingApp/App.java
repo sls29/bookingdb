d3b039f9eee444911c8bef34bdf5df1fee492774
roger <seles.lucian@gmail.com>
2023-05-17 13:59:12 +0300

    first

--- src/main/java/bookingApp/App.java
+++ src/main/java/bookingApp/App.java
@@ -13,16 +13,45 @@ public class App {
     static final String USER = "postgres";
     static final String PASSWORD = "root";
 
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws Exception {
 
         Connection conn = null;
-        Statement stmt = null;
+       Statement stmt = null;
         PreparedStatement preparedStatement = null;
 
         try {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
             stmt = conn.createStatement();
+
+            String sql = "INSERT INTO accommodation VALUES(?, ?, ?, ?, ?)";
+
+            preparedStatement = conn.prepareStatement(sql);
+
+//            preparedStatement.setInt(1, 1);
+//            preparedStatement.setString(2, "apartment");
+//            preparedStatement.setString(3, "double bed");
+//            preparedStatement.setInt(4, 2);
+//            preparedStatement.setString(5, "beach front");
+//            preparedStatement.addBatch();
+//
+//            preparedStatement.setInt(1, 2);
+//            preparedStatement.setString(2, "apartment");
+//            preparedStatement.setString(3, "queen size bed");
+//            preparedStatement.setInt(4, 2);
+//            preparedStatement.setString(5, "beach front, AC");
+//            preparedStatement.addBatch();
+
+            preparedStatement.setInt(1, 3);
+            preparedStatement.setString(2, "penthouse");
+            preparedStatement.setString(3, "king size bed");
+            preparedStatement.setInt(4, 2);
+            preparedStatement.setString(5, "beach front, AC, room service");
+            preparedStatement.addBatch();
+
+            int[] updateCounts = preparedStatement.executeBatch();
+//            conn.commit();
+//            conn.setAutoCommit(true);
 
 
         } catch (SQLException e) {
